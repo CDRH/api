b78ec696ac279ef5af118ea5c3734a90a3f6faae
Jessica Dussault <jdussault4@gmail.com>
2017-02-09 13:19:37 -0600

    sets up / and /collections responses
    lots of questions and TODOs, particularly for collections index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'default#root', as: :home
+  get '/collections' => 'collection#index', as: :collections
 end
