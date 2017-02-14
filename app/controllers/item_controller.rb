cd90ccc3c0746848a99191640f1962355568b459
Jessica Dussault <jdussault4@gmail.com>
2017-02-14 13:21:25 -0600

    facets, sorting, highlighting hacked in

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -17,7 +17,7 @@ class ItemController < ApplicationController
     # f[]
     # facet[]
     # hl
-    # min
+    # min  # TODO implement
     # num
     # sort
     # sort_desc
@@ -26,9 +26,20 @@ class ItemController < ApplicationController
     start = params["start"].blank? ? START : params["start"]
     num = params["num"].blank? ? NUM : params["num"]
     req = {
+      "aggs" => {},
       "from" => start,
+      # always include highlights by default
+      "highlight" => {
+        "fields" => {
+          "cdrh-text" => {
+            "fragment_size" => 100, "number_of_fragments" => 3
+          }
+        }
+      },
       "size" => num,
-      "query" => {}
+      # sort by _score by default
+      "sort" => ["_score"],
+      "query" => {},
     }
     bool = {}
 
@@ -45,6 +56,18 @@ class ItemController < ApplicationController
       bool["must"] = { "match_all" => {} }
     end
 
+    # FACETS[]
+    if !params["facet"].blank?
+      aggs = {}
+      params["facet"].each do |f|
+        # TODO the mapping is incorrect for keywords
+        # they should be keywords ONLY and not a multitype
+        # the multitype requires the .keyword seen below
+        aggs[f] = { "terms" => { "field" => "#{f}.keyword" } }
+      end
+      req["aggs"] = aggs
+    end
+
     # FILTER FIELDS F[]
     if !params["f"].blank?
       fields = params["f"]
@@ -54,13 +77,33 @@ class ItemController < ApplicationController
         filter << { "term" => { pair[0] => pair[1] } }
       end
       bool["filter"] = filter
-      req["query"]["bool"] = bool
     end
+
+    # HIGHLIGHT
+    if !params["hl"].blank? && params["hl"] == "false"
+      # remove highlighting from request if they don't want it
+      req.delete("highlight")
+    end
+
+    # SORT
+    sort_asc = params["sort"].blank? ? nil : params["sort"]
+    sort_desc = params["sort_desc"].blank? ? nil : params["sort_desc"]
+    if sort_asc || sort_desc
+      # default to using asc if sent both sort and sort_desc params
+      # TODO is this the way we want to handle sorting?
+      dir = sort_asc ? "asc" : "desc"
+      sort_field = sort_asc ? sort_asc : sort_desc
+      sort = { "#{sort_field}.keyword" => { "order" => dir } }
+      req["sort"].unshift(sort)
+    end
+
+    req["query"]["bool"] = bool
 
     begin
       puts req
       res = RestClient.post("#{ES_URI}/_search", req.to_json, { "content-type" => "json" })
       body = JSON.parse(res.body)
+      # TODO will need to correctly format this for api expected results
       render json: JSON.pretty_generate(body)
     rescue => e
       puts "ERROR: #{e}"
