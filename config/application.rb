7303e5da676aa1990501fdfb08b2debf56b1753f
Greg Tunink <techgique@gmail.com>
2018-11-20 16:20:13 -0600

    Drop redundant loading of initializer

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-require_relative "initializers/version"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
